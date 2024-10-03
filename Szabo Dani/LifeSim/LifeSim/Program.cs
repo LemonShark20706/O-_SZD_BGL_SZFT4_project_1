6c695906b65c68b3973223f214012da63905d158
Gold dt <82865840+Gold-dt@users.noreply.github.com>
2024-10-03 10:17:38 +0200

    AlapKezelés

---
+++ Szabo Dani/LifeSim/LifeSim/Program.cs
@@ -0,0 +1,68 @@
+﻿using LifeSimLib;
+using System;
+
+namespace LifeSim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] FuMatrix = new int[24, 120];
+
+            
+
+            FuNoves grow = new(3,FuMatrix,1);
+            Display(FuMatrix);
+            
+            while (true)
+            {
+                Display(FuMatrix);
+                for (int i = 0; i < FuMatrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < FuMatrix.GetLength(1); j++)
+                    {
+                        grow = new(i, j, FuMatrix);
+                    }
+                }
+            }
+
+
+           
+
+
+
+
+
+
+            void Display(int[,] Matrix)
+            {
+                Console.Clear();
+                for (int i = 0; i < Matrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < Matrix.GetLength(1); j++)
+                    {
+                        if(Matrix[i, j] == 1)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                        }
+                        else if (Matrix[i,j] == 2)
+                        {
+                            Console.ForegroundColor= ConsoleColor.Yellow;
+                        }
+                        else if (Matrix[i, j] == 3)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                        }
+                        else
+                        {
+                            Console.ResetColor();
+                        }
+                        Console.Write(Matrix[i, j]);
+                    }
+                    Console.WriteLine();
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Szabo Dani/LifeSim/LifeSimLib/FuNoves.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LifeSimLib
+{
+    public class FuNoves
+    {
+        public int MaxGrow {  get; set; }
+        Random random = new Random();
+
+
+        public FuNoves(int x,int y, int[,] matrix)
+        {
+            if(random.Next(0,50) == 0)
+            {
+                if (matrix[x,y] < 3)
+                {
+                    matrix[x, y] += 1;
+                }
+            }
+
+
+        }
+        public FuNoves(int grow, int[,]matrix,int def)
+        {
+            MaxGrow = grow;
+
+            for (int i = 0; i < 24; i++)
+            {
+                for (int j = 0; j < 120; j++)
+                {
+                    matrix[i, j] = def;
+                }
+            }
+        }
+    }
+}
